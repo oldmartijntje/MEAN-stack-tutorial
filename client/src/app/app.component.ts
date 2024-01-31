571b9fcdc4d05e3852b8ea36d73f4342629ebf1e
oldmartijntje <oldmartijntje@gmail.com>
2024-01-31 10:31:22 +0100

    it works

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-root',
     template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet></router-outlet>
-  `,
+    <div class="container-md">
+        <router-outlet></router-outlet>
+    </div>
+    `,
     styles: []
 })
 export class AppComponent {
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeesListComponent } from './employees-list/employees-list.component';
+import { EmployeeFormComponent } from './employee-form/employee-form.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { EditEmployeeComponent } from './edit-employee/edit-employee.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        EmployeesListComponent,
+        EmployeeFormComponent,
+        AddEmployeeComponent,
+        EditEmployeeComponent
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+        HttpClientModule,
+        ReactiveFormsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
